cc2f5598453bd892fc19c8b4e28ce585d7c26ae1
Duncan Applegarth <duncana513@gmail.com>
2023-08-08 18:32:20 -0700

    [No JIRA] Update deps

--- vite.config.ts
+++ vite.config.ts
@@ -8,13 +8,23 @@ const config: UserConfig = {
     // need to enable globals for auto-cleanup of rendered DOM resources
     globals: true,
     environment: 'jsdom',
-    setupFiles: ['./setupTest.ts'],
+    setupFiles: ['setupTest.ts'],
     coverage: {
       exclude: ['setupTest.ts']
     },
     include: ['src/**/*.{test,spec}.{js,ts}'],
     exclude: [...configDefaults.exclude, 'tests'],
     testTimeout: 10000
+  },
+  // see https://github.com/vitest-dev/vitest/issues/3866
+  // TLDR: need to explicitly allow files to be used for tests
+  // that are outside of project root (src)
+  server: {
+    fs: {
+      allow: process.env.VITEST
+        ? ['setupTest.ts', 'tests/util/storeMocks.ts', 'tests/util/testUtil.ts']
+        : undefined
+    }
   }
 };
 
