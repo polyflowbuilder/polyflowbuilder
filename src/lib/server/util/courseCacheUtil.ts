7521f3c9cd8f3abe82634306ae62abf0acdd5f89
Duncan Applegarth <duncana513@gmail.com>
2023-08-22 22:52:58 -0700

    update courseCacheUtil and /api/user/data/getUserFlowcharts
    * now, neither of these components require the use of monolithic apiData
    * introduce additional option 'includeProgramMetadata' in endpoint to
      return associated program metadata with flowcharts
    * courseCacheUtil introduces call to DB to grab courses for each
      flowchart in the user account. this is the correct way to do it but it
      can be slow, so a graular caching layer (likely Redis) will be needed
      to speed the request up in the future (these requests cannot be fully
      cached).

--- src/lib/server/util/courseCacheUtil.ts
+++ src/lib/server/util/courseCacheUtil.ts
@@ -1,13 +1,19 @@
-import { apiData } from '$lib/server/config/apiDataConfig';
+import { Prisma } from '@prisma/client';
+import { prisma } from '$lib/server/db/prisma';
 import { getCatalogFromProgramIDIndex } from '$lib/common/util/courseDataUtilCommon';
 import type { Flowchart } from '$lib/common/schema/flowchartSchema';
+import type { APICourse, Program } from '@prisma/client';
 import type { APICourseFull, CourseCache } from '$lib/types';
 
-export function generateCourseCacheFlowchart(flowchart: Flowchart): CourseCache[] {
-  const flowchartCourseCache: CourseCache[] = apiData.catalogs.map((catalog) => ({
-    catalog,
-    courses: []
-  }));
+export async function generateCourseCacheFlowchart(
+  flowchart: Flowchart,
+  programCache: Program[]
+): Promise<CourseCache[]> {
+  const catalogs = [...new Set(programCache.map((prog) => prog.catalog))];
+  const courseCacheSets: Set<APICourseFull>[] = catalogs.map(() => new Set());
+
+  // gather courses that need to be fetched
+  const courseIds: Prisma.Sql[] = [];
 
   flowchart.termData.forEach((termData) => {
     termData.courses.forEach((c) => {
@@ -18,51 +24,93 @@ export function generateCourseCacheFlowchart(flowchart: Flowchart): CourseCache[
         const courseCatalog = getCatalogFromProgramIDIndex(
           c.programIdIndex ?? 0,
           flowchart.programId,
-          apiData.programData
+          programCache
         );
 
         if (!courseCatalog) {
           throw new Error('courseCacheUtil: undefined courseCatalog');
         }
 
-        // get the associated course data
-        const courseMetadata = apiData.courseData
-          .find((cache) => cache.catalog === courseCatalog)
-          ?.courses.find((crs) => crs.id === c.id);
-
-        const catalogCourseCache = flowchartCourseCache.find(
-          (cache) => cache.catalog === courseCatalog
-        )?.courses;
-
-        if (!catalogCourseCache) {
-          throw new Error('courseCacheUtil: undefined catalog in course cache');
-        }
-
-        if (courseMetadata) {
-          catalogCourseCache.push(courseMetadata);
-        }
+        courseIds.push(Prisma.sql`(${Prisma.join([c.id, courseCatalog])})`);
       }
     });
   });
 
+  // TODO: move this logic to an API route
+  // fetch the courses from the DB
+  // ternary to make sure we only query if we have courses to query for
+  const courses: APICourseFull[] = courseIds.length
+    ? (
+        await prisma.$queryRaw<
+          (APICourse & {
+            termSummer: number | null;
+            termFall: number | null;
+            termWinter: number | null;
+            termSpring: number | null;
+          })[]
+        >`SELECT * FROM Course LEFT JOIN TermTypicallyOffered USING (id, catalog) WHERE (id, catalog) IN (${Prisma.join(
+          courseIds
+        )})`
+      ).map((dbCourseDataRaw) => {
+        const { termSummer, termFall, termWinter, termSpring, ...crs } = dbCourseDataRaw;
+
+        // if no tto data is present, all four entries will be null, so just pick one to check
+        return {
+          ...crs,
+          uscpCourse: !!crs.uscpCourse,
+          gwrCourse: !!crs.gwrCourse,
+          dynamicTerms:
+            termSummer === null
+              ? null
+              : {
+                  termSummer: !!termSummer,
+                  termFall: !!termFall,
+                  termWinter: !!termWinter,
+                  termSpring: !!termSpring
+                }
+        };
+      })
+    : [];
+
+  // map courses to course cache
+  courses.forEach((crs) => {
+    const idx = catalogs.findIndex((catalog) => catalog === crs.catalog);
+    if (idx === -1) {
+      throw new Error('courseCacheUtil: undefined catalog in courseCache Set');
+    }
+
+    courseCacheSets[idx].add(crs);
+  });
+
+  const flowchartCourseCache: CourseCache[] = [];
+  catalogs.forEach((catalog, i) =>
+    flowchartCourseCache.push({
+      catalog,
+      courses: Array.from(courseCacheSets[i])
+    })
+  );
   return flowchartCourseCache;
 }
 
-export function generateUserCourseCache(userFlowcharts: Flowchart[]): CourseCache[] {
-  const courseCacheSets: Set<APICourseFull>[] = apiData.catalogs.map(() => new Set());
+export async function generateUserCourseCache(
+  userFlowcharts: Flowchart[],
+  programCache: Program[]
+): Promise<CourseCache[]> {
+  const catalogs = [...new Set(programCache.map((prog) => prog.catalog))];
+  const courseCacheSets: Set<APICourseFull>[] = catalogs.map(() => new Set());
 
   // TODO: can we optimize this? O(n^3)
-  userFlowcharts.forEach((flow) => {
-    const flowchartCourseCacheData = generateCourseCacheFlowchart(flow);
+  for await (const flow of userFlowcharts) {
+    const flowchartCourseCacheData = await generateCourseCacheFlowchart(flow, programCache);
     flowchartCourseCacheData.forEach((flowchartCourseCacheDataCatalog, i) => {
       flowchartCourseCacheDataCatalog.courses.forEach((crs) => {
         courseCacheSets[i].add(crs);
       });
     });
-  });
+  }
 
   const userCourseCache: CourseCache[] = [];
-  apiData.catalogs.forEach((catalog, i) =>
+  catalogs.forEach((catalog, i) =>
     userCourseCache.push({
       catalog,
       courses: Array.from(courseCacheSets[i])
