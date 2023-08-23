a79161ecaf53cca47e06d37f1bd65498bed2d559
Duncan Applegarth <duncana513@gmail.com>
2023-08-23 00:56:25 -0700

    update /flows page to bring down programMetadata when getting user flows
    * once the old programData load on +layout is removed, can use this to
      start the programData cache on the frontend

--- src/routes/flows/+page.ts
+++ src/routes/flows/+page.ts
@@ -1,3 +1,4 @@
+import type { Program } from '@prisma/client';
 import type { PageLoad } from './$types';
 import type { Flowchart } from '$lib/common/schema/flowchartSchema';
 import type { CourseCache } from '$lib/types';
@@ -5,7 +6,9 @@ import type { CourseCache } from '$lib/types';
 export const load: PageLoad = async ({ fetch }) => {
   // get flowcharts with course cache
   // if we include multiple requests, make sure they are using the promise streaming SK flow
-  const userFlowcharts = await fetch('/api/user/data/getUserFlowcharts?includeCourseCache=true');
+  const userFlowcharts = await fetch(
+    '/api/user/data/getUserFlowcharts?includeCourseCache=true&includeProgramMetadata=true'
+  );
   if (!userFlowcharts.ok) {
     // need this explicitly instead of null to ensure types are inferred correctly
     return undefined;
@@ -14,11 +17,13 @@ export const load: PageLoad = async ({ fetch }) => {
       message: string;
       flowcharts: Flowchart[];
       courseCache: CourseCache[];
+      programMetadata: Program[][];
     };
 
     return {
       flowcharts: userFlowchartsJson.flowcharts,
-      courseCache: userFlowchartsJson.courseCache
+      courseCache: userFlowchartsJson.courseCache,
+      programMetadata: userFlowchartsJson.programMetadata
     };
   }
 };
