1ad8ef30aad89ea6761b32232db7f02431f7e1e4
Duncan Applegarth <duncana513@gmail.com>
2023-08-24 12:52:31 -0700

    update getTemplateFlowcharts to return associated program metadata
    * straightforward bc TemplateFlowchart table has Program FK relation

--- src/lib/server/db/templateFlowchart.ts
+++ src/lib/server/db/templateFlowchart.ts
@@ -1,19 +1,35 @@
 import { prisma } from '$lib/server/db/prisma';
 import { initLogger } from '$lib/common/config/loggerConfig';
-import type { TemplateFlowchart } from '@prisma/client';
+import type { Program, TemplateFlowchart } from '@prisma/client';
 
 const logger = initLogger('DB/TemplateFlowchart');
 
-export async function getTemplateFlowcharts(programId: string[]): Promise<TemplateFlowchart[]> {
+export async function getTemplateFlowcharts(programId: string[]): Promise<
+  {
+    flowchart: TemplateFlowchart;
+    programMetadata: Program;
+  }[]
+> {
   const res = await prisma.templateFlowchart.findMany({
     where: {
       programId: {
         in: programId
       }
+    },
+    include: {
+      programIdRelation: true
     }
+  });
+
+  const resConverted = res.map((templateFlowData) => {
+    const { programIdRelation: programMetadata, ...templateFlowchart } = templateFlowData;
+    return {
+      flowchart: templateFlowchart,
+      programMetadata
+    };
   });
 
   logger.info(`Successfully got template flowchart(s) ${programId.join(',')}`);
 
-  return res;
+  return resConverted;
 }
