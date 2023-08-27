d00484bed8f61fe3207d9e2cb32d163daa1c137f
Duncan Applegarth <duncana513@gmail.com>
2023-08-26 20:13:40 -0700

    create mocked stores required for ProgramSelector tests

--- tests/util/storeMocks.ts
+++ tests/util/storeMocks.ts
@@ -5,38 +5,35 @@
 import { writable } from 'svelte/store';
 import type { Program } from '@prisma/client';
 import type { Flowchart } from '$lib/common/schema/flowchartSchema';
-import type { CourseCache } from '$lib/types';
+import type { MajorNameCache, CourseCache } from '$lib/types';
 
 // set stores
+const mockAvailableFlowchartStartYearsWritable = writable<string[]>([]);
+const mockAvailableFlowchartCatalogsWritable = writable<string[]>([]);
 const mockProgramCacheWritable = writable<Program[]>([]);
 const mockCourseCacheWritable = writable<CourseCache[]>([]);
+const mockMajorNameCacheWritable = writable<MajorNameCache[]>([]);
+const mockCatalogMajorNameCacheWritable = writable<Set<string>>(new Set<string>());
 const mockModalOpenWritable = writable<boolean>(false);
 const mockSelectedFlowIndexWritable = writable<number>(-1);
 const mockUserFlowchartsWritable = writable<Flowchart[]>([]);
 
-export const mockProgramCacheStore = {
-  subscribe: mockProgramCacheWritable.subscribe,
-  set: mockProgramCacheWritable.set
-};
+// apiDataStore mocks
 
-export const mockCourseCacheStore = {
-  subscribe: mockCourseCacheWritable.subscribe,
-  set: mockCourseCacheWritable.set
-};
+// API data
+export const mockAvailableFlowchartStartYearsStore = mockAvailableFlowchartStartYearsWritable;
+export const mockAvailableFlowchartCatalogsStore = mockAvailableFlowchartCatalogsWritable;
+
+// caches
+export const mockProgramCacheStore = mockProgramCacheWritable;
+export const mockCourseCacheStore = mockCourseCacheWritable;
+export const mockMajorNameCacheStore = mockMajorNameCacheWritable;
+export const mockCatalogMajorNameCacheStore = mockCatalogMajorNameCacheWritable;
 
 // general-purpose mock modal store since we will only be
 // using this mock for one store at a time
-export const mockModalOpenStore = {
-  subscribe: mockModalOpenWritable.subscribe,
-  set: mockModalOpenWritable.set
-};
+export const mockModalOpenStore = mockModalOpenWritable;
 
-export const mockSelectedFlowIndexStore = {
-  subscribe: mockSelectedFlowIndexWritable.subscribe,
-  set: mockSelectedFlowIndexWritable.set
-};
+export const mockSelectedFlowIndexStore = mockSelectedFlowIndexWritable;
 
-export const mockUserFlowchartsStore = {
-  subscribe: mockUserFlowchartsWritable.subscribe,
-  set: mockUserFlowchartsWritable.set
-};
+export const mockUserFlowchartsStore = mockUserFlowchartsWritable;
