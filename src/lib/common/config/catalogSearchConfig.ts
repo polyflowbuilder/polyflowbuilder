d4a636214e31c58b265e311e4f3674f6e76f7403
Duncan Applegarth <duncana513@gmail.com>
2023-08-28 13:33:24 -0700

    remove fuzzy searching parameters from config

--- src/lib/common/config/catalogSearchConfig.ts
+++ src/lib/common/config/catalogSearchConfig.ts
@@ -1,6 +1,7 @@
 // catalog search parameters
 export const SEARCH_DELAY_TIME_MS = 500;
 export const MAX_SEARCH_RESULTS_RETURN_COUNT = 24;
-export const FUZZY_SEARCH_THRESOLD = 0.6;
+
+// TODO: doesn't belong here
 export const SANITIZE_REGEX = (input: string): string =>
   input.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
