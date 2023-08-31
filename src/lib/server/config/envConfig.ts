91fc257d7680967f76b03be9e21711c4899076ca
Duncan Applegarth <duncana513@gmail.com>
2023-08-30 20:11:09 -0700

    disable initialization of apiData on app startup

--- src/lib/server/config/envConfig.ts
+++ src/lib/server/config/envConfig.ts
@@ -1,6 +1,5 @@
 import dotenv from 'dotenv';
 import * as nodeMailerConfig from '$lib/server/config/nodeMailerConfig';
-import * as apiDataConfig from '$lib/server/config/apiDataConfig';
 import { execSync } from 'child_process';
 import { initLogger } from '$lib/common/config/loggerConfig';
 
@@ -11,7 +10,7 @@ export let FULL_VERSION_STRING = 'unknown';
 // session max age in seconds
 export const SESSION_MAX_AGE = 60 * 60 * 24;
 
-export async function loadEnv(): Promise<void> {
+export function loadEnv() {
   // env stuff specific to server
   try {
     const commit = execSync('git rev-parse --short HEAD').toString().slice(0, -1);
@@ -38,7 +37,6 @@ export async function loadEnv(): Promise<void> {
 
   // setup things that depend on env vars here
   nodeMailerConfig.init();
-  await apiDataConfig.init();
 
   logger.info('PolyFlowBuilder environment initialization complete');
 }
