6f1b29e1d749cc572ac8284b336963fab7a44a0e
Duncan Applegarth <duncana513@gmail.com>
2023-09-13 00:06:41 -0700

    implement sendEmail() using email api

--- src/lib/server/util/emailUtil.ts
+++ src/lib/server/util/emailUtil.ts
@@ -1,5 +1,58 @@
 import { initLogger } from '$lib/common/config/loggerConfig';
+import type { EmailTemplateData } from '$lib/types/emailTemplateTypes';
 
 const logger = initLogger('Util/EmailUtil');
 
-export function sendEmail() {}
+export async function sendEmail(template: EmailTemplateData, to: string, subject: string) {
+  // build email payload
+  const payload = {
+    // request is valid for 1 minute
+    expiry: Date.now() + 60000,
+    to,
+    from: process.env.EMAIL_SENDER,
+    subject,
+    template
+  };
+  const stringPayload = JSON.stringify(payload);
+
+  // build signed request
+  const encoder = new TextEncoder();
+
+  // import private key
+  const key = await crypto.subtle.importKey(
+    'raw',
+    encoder.encode(process.env.CF_EMAIL_API_PRIV_KEY),
+    {
+      name: 'HMAC',
+      hash: 'SHA-256'
+    },
+    false,
+    ['sign']
+  );
+
+  // sign data
+  const signatureBuffer = await crypto.subtle.sign('HMAC', key, encoder.encode(stringPayload));
+  const signature = Buffer.from(signatureBuffer).toString('base64');
+
+  // send the request
+  if (!process.env.CF_EMAIL_API_ENDPOINT) {
+    throw new Error('sendEmail: CF_EMAIL_API_ENDPOINT is not defined');
+  }
+  logger.info(`Attempting to send '${template.name}' email via email endpoint`);
+  const res = await fetch(process.env.CF_EMAIL_API_ENDPOINT, {
+    method: 'POST',
+    body: JSON.stringify({
+      signature,
+      data: stringPayload
+    }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+  if (!res.ok) {
+    // get error
+    const resData = JSON.stringify(await res.json());
+    throw new Error(`sendEmail: failed to send email, status [${res.status}], detail [${resData}]`);
+  }
+  logger.info('Email send successful');
+}
