47de5f52dabf729bae12dc4ab3dd70b6eaf6ac81
Duncan Applegarth <duncana513@gmail.com>
2023-09-14 12:52:32 -0700

    remove file rotation logging mechanism

--- src/lib/common/config/loggerConfig.ts
+++ src/lib/common/config/loggerConfig.ts
@@ -1,5 +1,4 @@
 import util from 'util';
-import DailyRotateFile from 'winston-daily-rotate-file';
 import { format, createLogger, transports } from 'winston';
 import type { LoggerOptions } from 'winston';
 
@@ -15,23 +14,10 @@ const combineMessageAndSplatFormat = format((info) => {
   return info;
 });
 
-const dailyLogFileRotationTransport: DailyRotateFile = new DailyRotateFile({
-  filename: 'polyflowbuilder-%DATE%.log',
-  // NOTE: in prod, will use fallback bc env vars are not loaded early enough in envConfig
-  // for LOG_DIRECTORY to be available -- will probably address in the future
-  dirname: process.env.LOG_DIRECTORY ?? 'logs',
-  zippedArchive: true,
-  auditFile: 'loghistory.json',
-  createSymlink: true
-});
-
 function createLoggerConfig(source: string): LoggerOptions {
   return {
     level: 'verbose',
-    transports:
-      process.env.NODE_ENV === 'production'
-        ? [dailyLogFileRotationTransport]
-        : [new transports.Console()],
+    transports: [new transports.Console()],
     format: format.combine(
       combineMessageAndSplatFormat(),
       format.timestamp({
