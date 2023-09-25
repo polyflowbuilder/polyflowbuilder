57119d8c7836dc75d5bb49a40ebe1366087eccd4
Duncan Applegarth <duncana513@gmail.com>
2023-09-25 00:24:58 -0700

    add welcomeModal modal state

--- src/lib/client/stores/modalStateStore.ts
+++ src/lib/client/stores/modalStateStore.ts
@@ -1,8 +1,12 @@
 // where modal state will be stored
 import { writable } from 'svelte/store';
 
+// functional Flows modals
 export const newFlowModalOpen = writable(false);
 export const addTermsModalOpen = writable(false);
 export const deleteTermsModalOpen = writable(false);
 export const editFlowPropertiesModalOpen = writable(false);
 export const customizeCoursesModalOpen = writable(false);
+
+// information Flows modals
+export const welcomeModalOpen = writable(false);
