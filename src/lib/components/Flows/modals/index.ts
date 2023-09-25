75bd3581c1cf0e76e69a29a923415ae492067223
Duncan Applegarth <duncana513@gmail.com>
2023-09-25 03:35:51 -0700

    update import order

--- src/lib/components/Flows/modals/index.ts
+++ src/lib/components/Flows/modals/index.ts
@@ -1,18 +1,18 @@
+import WelcomeModal from './WelcomeModal.svelte';
 import NewFlowModal from './NewFlowModal.svelte';
 import AddTermsModal from './AddTermsModal.svelte';
 import DeleteTermsModal from './DeleteTermsModal.svelte';
 import EditFlowPropertiesModal from './EditFlowPropertiesModal.svelte';
 import CustomizeCourseModal from './CustomizeCourseModal.svelte';
-import WelcomeModal from './WelcomeModal.svelte';
 
 import ModalWrapper from './ModalWrapper.svelte';
 
 export {
+  WelcomeModal,
   NewFlowModal,
   AddTermsModal,
   DeleteTermsModal,
   EditFlowPropertiesModal,
   CustomizeCourseModal,
-  WelcomeModal,
   ModalWrapper
 };
