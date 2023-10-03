124a673599962f463e3a86c805e1ef11bc30bdcc
Duncan Applegarth <duncana513@gmail.com>
2023-10-03 15:55:49 -0700

    add playwright projects for all major browsers

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,3 +1,4 @@
+import { devices } from '@playwright/test';
 import type { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
@@ -7,7 +8,43 @@ const config: PlaywrightTestConfig = {
     timeout: 120000
   },
   testDir: 'tests',
-  retries: 3
+  retries: 3,
+  projects: [
+    {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome']
+      }
+    },
+    {
+      name: 'chrome',
+      use: {
+        ...devices['Desktop Chrome'],
+        channel: 'chrome'
+      }
+    },
+    {
+      name: 'edge',
+      use: {
+        ...devices['Desktop Edge'],
+        channel: 'msedge'
+      }
+    },
+    {
+      name: 'firefox',
+      use: {
+        ...devices['Desktop Firefox']
+      }
+    },
+    {
+      name: 'webkit',
+      use: {
+        ...devices['Desktop Safari'],
+        // see https://github.com/microsoft/playwright/issues/24457
+        deviceScaleFactor: 1
+      }
+    }
+  ]
 };
 
 export default config;
