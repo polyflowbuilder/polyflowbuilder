3be595b67596fbde8109e020971a0fd7ebb2e072
Duncan Applegarth <duncana513@gmail.com>
2023-10-03 22:44:23 -0700

    add char counter to feedback text submission
    users were getting confused when the textbox would stop accepting input

--- src/routes/feedback/+page.svelte
+++ src/routes/feedback/+page.svelte
@@ -7,6 +7,7 @@
   export let form: ActionData;
 
   let loading = false;
+  let feedbackText = '';
 </script>
 
 <main class="container mx-auto grid grid-cols-6 gap-4">
@@ -36,6 +37,7 @@
           return ({ update }) => {
             void (async () => {
               loading = false;
+              feedbackText = '';
               await update();
             })();
           };
@@ -74,7 +76,15 @@
             >
           {/if}
 
-          <label for="feedback" class="font-semibold label label-text text-lg">Feedback</label>
+          <label for="feedback" class="font-semibold label label-text text-lg">
+            <span>Feedback</span>
+            <span
+              class="text-sm font-normal"
+              class:text-red-600={!feedbackText.length}
+              class:text-green-700={feedbackText.length}
+              >({feedbackText.length}/{FEEDBACK_MESSAGE_MAX_LENGTH})
+            </span>
+          </label>
           <textarea
             class="textarea h-56 textarea-bordered resize-none"
             id="feedback"
@@ -82,6 +92,7 @@
             placeholder="Enter your feedback here!"
             maxlength={FEEDBACK_MESSAGE_MAX_LENGTH}
             required
+            bind:value={feedbackText}
           />
           {#if form?.feedbackValidationErrors?.feedback}
             <small id="feedbackError" class="text-red-600 label label-text-alt"
@@ -89,7 +100,11 @@
             >
           {/if}
 
-          <button class="btn btn-accent btn-block mt-6" disabled={loading} type="submit">
+          <button
+            class="btn btn-accent btn-block mt-6"
+            disabled={loading || !feedbackText.length}
+            type="submit"
+          >
             <span class={loading ? 'loading loading-spinner' : ''} />
             Submit Feedback
           </button>
