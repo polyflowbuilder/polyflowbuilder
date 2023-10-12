c2d0ca5b23a536c586af1143300f838a1d19ee59
Duncan Applegarth <duncana513@gmail.com>
2023-10-12 15:48:29 -0700

    add ObjectSet class
    * behaves like a Set, but for deep objects

---
+++ src/lib/common/util/ObjectSet.ts
@@ -0,0 +1,50 @@
+// ObjectSet class behaves like a set, but for deep objects
+// takes a function to map objects of a certain type to a string ID,
+// used as the key in the internal Map
+
+export class ObjectSet<T> {
+  #itemMap: Map<string, T>;
+  #keyFunction: (input: T) => string;
+
+  constructor(items: T[] = [], keyFunction: (input: T) => string) {
+    this.#itemMap = new Map<string, T>();
+    this.#keyFunction = keyFunction;
+
+    for (const item of items) {
+      this.add(item);
+    }
+  }
+
+  get size() {
+    return this.#itemMap.size;
+  }
+
+  add(item: T) {
+    const key = this.#keyFunction(item);
+    if (!this.#itemMap.has(key)) {
+      this.#itemMap.set(key, item);
+    }
+  }
+
+  get(key: string) {
+    return this.#itemMap.get(key);
+  }
+
+  clear() {
+    this.#itemMap.clear();
+  }
+
+  delete(item: T) {
+    const key = this.#keyFunction(item);
+    return this.#itemMap.delete(key);
+  }
+
+  values() {
+    return this.#itemMap.values();
+  }
+
+  has(item: T) {
+    const key = this.#keyFunction(item);
+    return this.#itemMap.has(key);
+  }
+}
