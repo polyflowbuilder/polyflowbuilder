baaf84fbda25a1c2375e0427d102a6e25a703061
Duncan Applegarth <duncana513@gmail.com>
2023-10-14 00:51:34 -0700

    update searchCache store to use new ObjectMap

--- src/lib/client/stores/catalogSearchStore.ts
+++ src/lib/client/stores/catalogSearchStore.ts
@@ -1,5 +1,8 @@
 import { writable } from 'svelte/store';
+import { ObjectMap } from '$lib/common/util/ObjectMap';
 import type { CatalogSearchResults, SearchCache } from '$lib/types';
 
-export const searchCache = writable<SearchCache[]>([]);
+export const searchCache = writable<SearchCache>(
+  new ObjectMap((k) => `${k.catalog}|${k.field}|${k.query}`)
+);
 export const activeSearchResults = writable<Promise<CatalogSearchResults> | undefined>();
