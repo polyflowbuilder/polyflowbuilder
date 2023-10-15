3a9fd89279b0337d2f22f4f18d797ef54540d6b5
Duncan Applegarth <duncana513@gmail.com>
2023-10-15 00:37:04 -0700

    prefer default keyFunction for searchCache ObjectMap

--- src/lib/client/stores/catalogSearchStore.ts
+++ src/lib/client/stores/catalogSearchStore.ts
@@ -2,7 +2,5 @@ import { writable } from 'svelte/store';
 import { ObjectMap } from '$lib/common/util/ObjectMap';
 import type { CatalogSearchResults, SearchCache } from '$lib/types';
 
-export const searchCache = writable<SearchCache>(
-  new ObjectMap((k) => `${k.catalog}|${k.field}|${k.query}`)
-);
+export const searchCache = writable<SearchCache>(new ObjectMap());
 export const activeSearchResults = writable<Promise<CatalogSearchResults> | undefined>();
