427f144b728d29ad604832999b081fb32363f0d3
Duncan Applegarth <duncana513@gmail.com>
2023-10-16 15:21:53 -0700

    update tests to work with error checking

--- src/lib/components/Flows/FlowEditor/TermContainer.test.ts
+++ src/lib/components/Flows/FlowEditor/TermContainer.test.ts
@@ -1,11 +1,33 @@
-import TermContainer from './TermContainer.svelte';
+import * as apiDataConfig from '$lib/server/config/apiDataConfig';
 import { render, screen } from '@testing-library/svelte';
 import { TEST_FLOWCHART_SINGLE_PROGRAM_2 } from '../../../../../tests/util/testFlowcharts';
+import { mockCourseCacheStore, mockProgramCacheStore } from '../../../../../tests/util/storeMocks';
+
+// load necessary API data
+await apiDataConfig.init();
 
 const TEST_TERM_EMPTY = TEST_FLOWCHART_SINGLE_PROGRAM_2.termData[0];
 const TEST_TERM_COURSES = TEST_FLOWCHART_SINGLE_PROGRAM_2.termData[1];
 
+// this import NEEDS to be down here or else the vi.mock() call that we're using to mock
+// the programCache and courseCache stores FAILS!! because vi.mock() MUST be called
+// before the TermContainer component is imported or else things break
+import TermContainer from './TermContainer.svelte';
+
 describe('TermContainer component tests', () => {
+  // need to mock out relevant stores since TermContainer depends on these
+  // mock call is hoisted to top of file
+  beforeAll(() => {
+    vi.mock('$lib/client/stores/apiDataStore', () => {
+      return {
+        courseCache: mockCourseCacheStore,
+        programCache: mockProgramCacheStore
+      };
+    });
+    mockProgramCacheStore.set(apiDataConfig.apiData.programData);
+    mockCourseCacheStore.set(apiDataConfig.apiData.courseData);
+  });
+
   test('empty term', () => {
     render(TermContainer, {
       props: {
@@ -27,7 +49,7 @@ describe('TermContainer component tests', () => {
     render(TermContainer, {
       props: {
         flowId: '68be11b7-389b-4ebc-9b95-8997e7314497',
-        flowProgramId: ['68be11b7-389b-4ebc-9b95-8997e7314497'],
+        flowProgramId: ['4fc5d3c2-f5a7-4074-ae3d-5b3e01cb1168'],
         term: TEST_TERM_COURSES,
         termName: 'test term 2'
       }
