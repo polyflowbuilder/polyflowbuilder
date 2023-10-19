97f6f239d0a62fe00c7472c08694ddad5bccb618
Duncan Applegarth <duncana513@gmail.com>
2023-10-19 13:27:53 -0700

    set default value for maxWidth of tooltip

--- src/lib/client/util/tooltipUtil.ts
+++ src/lib/client/util/tooltipUtil.ts
@@ -1,5 +1,13 @@
 import tippy from 'tippy.js';
+import { MAX_TOOLTIP_WIDTH_PX } from '$lib/client/config/uiConfig';
 import type { Props } from 'tippy.js';
+
+function mergeDefaultTooltipParams(params: Partial<Props>): Partial<Props> {
+  return {
+    maxWidth: MAX_TOOLTIP_WIDTH_PX,
+    ...params
+  };
+}
 
 // actions: https://blog.logrocket.com/svelte-actions-introduction/
 export function tooltip(
@@ -9,10 +17,10 @@ export function tooltip(
   update?: (params: Partial<Props>) => void;
   destroy?: () => void;
 } {
-  const tip = tippy(node, params);
+  const tip = tippy(node, mergeDefaultTooltipParams(params));
   return {
     update: (newParams: Partial<Props>) => {
-      tip.setProps(newParams);
+      tip.setProps(mergeDefaultTooltipParams(newParams));
     },
     destroy: () => {
       tip.destroy();
