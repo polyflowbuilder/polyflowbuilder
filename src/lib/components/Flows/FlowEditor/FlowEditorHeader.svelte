652252187ae75ea54aba63cabd44dd0a29ebf847
Duncan Applegarth <duncana513@gmail.com>
2023-10-19 20:45:28 -0700

    add tooltips to FlowEditorHeader

--- src/lib/components/Flows/FlowEditor/FlowEditorHeader.svelte
+++ src/lib/components/Flows/FlowEditor/FlowEditorHeader.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
   import Fa from 'svelte-fa';
+  import { tooltip } from '$lib/client/util/tooltipUtil';
   import { viewingFlowInfoPanel } from '$lib/client/stores/UIDataStore';
   import { createEventDispatcher } from 'svelte';
   import { faArrowLeft, faArrowRight, faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
@@ -7,6 +8,11 @@
     TERM_CONTAINER_WIDTH_PX,
     FLOW_EDITOR_HEADER_PADDING_PX
   } from '$lib/client/config/uiConfig';
+  import {
+    viewingFlowInfoPanelTooltipConfig,
+    flowEditorLeftScrollTooltipConfig,
+    flowEditorRightScrollTooltipConfig
+  } from '$lib/client/config/flowEditorHeaderTooltipConfigClient';
 
   export let name: string;
   export let enableLeftScrollArrow = false;
@@ -34,6 +40,7 @@
       <button
         class="btn btn-sm btn-square btn-ghost"
         aria-label="show/hide flow info panel"
+        use:tooltip={viewingFlowInfoPanelTooltipConfig}
         on:click={() => ($viewingFlowInfoPanel = !$viewingFlowInfoPanel)}
       >
         <Fa icon={$viewingFlowInfoPanel ? faEye : faEyeSlash} />
@@ -42,6 +49,7 @@
         class="btn btn-sm btn-square btn-ghost"
         aria-label="flow editor left scroll"
         disabled={!enableLeftScrollArrow}
+        use:tooltip={flowEditorLeftScrollTooltipConfig}
         on:click={() => dispatch('leftScrollArrowClick')}
       >
         <Fa icon={faArrowLeft} />
@@ -50,6 +58,7 @@
         class="btn btn-sm btn-square btn-ghost"
         aria-label="flow editor right scroll"
         disabled={!enableRightScrollArrow}
+        use:tooltip={flowEditorRightScrollTooltipConfig}
         on:click={() => dispatch('rightScrollArrowClick')}
       >
         <Fa icon={faArrowRight} />
