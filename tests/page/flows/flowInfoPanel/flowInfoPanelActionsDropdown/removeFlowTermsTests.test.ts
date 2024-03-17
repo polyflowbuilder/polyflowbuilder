9a2c0b84357e304fa790e75906fca9b4e377486a
Duncan Applegarth <duncana513@gmail.com>
2024-03-17 15:30:08 -0700

    scaffold out tests for remove flow terms modal

---
+++ tests/page/flows/flowInfoPanel/flowInfoPanelActionsDropdown/removeFlowTermsTests.test.ts
@@ -0,0 +1,60 @@
+import { expect, test } from '@playwright/test';
+import { PrismaClient } from '@prisma/client';
+import { skipWelcomeMessage } from 'tests/util/frontendInteractionUtil';
+import { populateFlowcharts } from 'tests/util/userDataTestUtil';
+import { createUser, deleteUser } from '$lib/server/db/user';
+import { getUserEmailString, performLoginFrontend } from 'tests/util/userTestUtil';
+
+test.describe('remove flowchart terms tests', () => {
+  test.describe.configure({ mode: 'serial' });
+  const prisma = new PrismaClient();
+  let userId: string;
+  let userEmail: string;
+
+  // eslint-disable-next-line no-empty-pattern
+  test.beforeAll(async ({}, testInfo) => {
+    // create account
+    userEmail = getUserEmailString(
+      'pfb_test_flowsPage_remove_terms_modal_playwright@test.com',
+      testInfo
+    );
+    const id = await createUser({
+      email: userEmail,
+      username: 'test',
+      password: 'test'
+    });
+
+    if (!id) {
+      throw new Error('userId is null');
+    }
+
+    userId = id;
+
+    // populate some flowcharts
+    await populateFlowcharts(prisma, userId, 2, [
+      {
+        idx: 0,
+        info: {
+          longTermCount: 0,
+          termCount: 6
+        }
+      },
+      {
+        idx: 1,
+        info: {
+          longTermCount: 0,
+          termCount: 10
+        }
+      }
+    ]);
+  });
+
+  test.beforeEach(async ({ page }) => {
+    await skipWelcomeMessage(page);
+    await performLoginFrontend(page, userEmail, 'test');
+  });
+
+  test.afterAll(async () => {
+    await deleteUser(userEmail);
+  });
+});
