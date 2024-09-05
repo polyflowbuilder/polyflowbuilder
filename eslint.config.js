e270596d518cda10e3ceccca77b31166a6e842ab
Duncan Applegarth <duncana513@gmail.com>
2024-09-05 00:53:53 -0700

    migrate to new flat config

---
+++ eslint.config.js
@@ -0,0 +1,53 @@
+import js from '@eslint/js';
+import ts from 'typescript-eslint';
+import path from 'path';
+import svelte from 'eslint-plugin-svelte';
+import globals from 'globals';
+import prettier from 'eslint-config-prettier';
+import { fileURLToPath } from 'url';
+import { includeIgnoreFile } from '@eslint/compat';
+
+// get .gitignore path
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const gitignorePath = path.resolve(__dirname, '.gitignore');
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  js.configs.recommended,
+  ...ts.configs.strictTypeChecked,
+  ...ts.configs.stylisticTypeChecked,
+  ...svelte.configs['flat/recommended'],
+  prettier,
+  ...svelte.configs['flat/prettier'],
+  {
+    languageOptions: {
+      parserOptions: {
+        extraFileExtensions: ['.svelte'],
+        project: true
+      },
+      globals: {
+        ...globals.browser,
+        ...globals.node
+      }
+    }
+  },
+  {
+    files: ['**/*.svelte'],
+    languageOptions: {
+      parserOptions: {
+        parser: ts.parser
+      }
+    }
+  },
+  includeIgnoreFile(gitignorePath),
+  {
+    ignores: [
+      // ignore config files at root level
+      '*.js',
+      '*.cjs',
+      '*.ts',
+      '*.mjs'
+    ]
+  }
+];
